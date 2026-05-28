8c64c2590016382c3b569279e07bd3e643285694
GAURAV722-ve <gsinghgv722@gmail.com>
2026-05-29 00:06:09 +0530

    Added API call project

--- API CALL/src/App.jsx
+++ API CALL/src/App.jsx
@@ -1,63 +1,27 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
 const App = () => {
-  const [title, setTitle] = useState('')
-  const [details, setDetails] = useState('')
-  const [Task, setTask] = useState([])
+  const [data, setData] = useState([])
 
-  const submitHandler = (e) => {
-    e.preventDefault()
-
-    const copy_task = [...Task];
-
-    copy_task.push({title,details})
-
-    setTask(copy_task)
-    console.log(Task)
-
-    setTitle('')
-    setDetails('')
+  const getData = async () => {
+    const response = await axios.get('https://picsum.photos/v2/list')
+    setData(response.data)
   }
 
   return (
-    <div className='h-screen lg:flex bg-black text-white min-h-screen'>
-      <form 
-        onSubmit={submitHandler}
-        className='flex justify-between lg:w-1/2 flex-col items-center p-10 gap-10'
-      >
-        <h1 className='text-3xl font-bold'>ADD NOTES</h1>
+    <div>
+      <button onClick={getData}>GET DATA</button>
 
-        <input 
-          type='text'
-          placeholder='Enter Notes Heading'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className='px-5 py-2 w-full border-2 rounded border-white text-white bg-transparent'
-        />
-
-        <textarea 
-          placeholder='Enter details'
-          value={details}
-          onChange={(e) => setDetails(e.target.value)}
-          className='px-5 py-2 w-full border-2 rounded border-white text-white bg-transparent h-40 font-medium'
-        />
-
-        <button 
-          className='bg-white active:bg-gray-300 text-black px-5 py-2 rounded hover:bg-gray-300'>
-          Add Note
-        </button>
-      </form>
-
-      <div className='lg:w-1/2 lg:border-l p-10'>
-        <h1 className='text-3xl font-bold py-1'>RECENT NOTES</h1>
-        <div className='flex flex-wrap items-start justify-start gap-5 mt-6 h-full overflow-auto'>
-          {Task.map(function(elem,idx){
-            return <div key={idx} className='h-52 w-40 rounded-xl text-black p-4 bg-white'>
-              <h3 className='text-center leading-tight text-xl font-bold'>{elem.title}</h3>
-              <p>{elem.details}</p>
+      <div>
+        {data.map((item) => {
+          return (
+            <div key={item.id}>
+              <h3>{item.author}</h3>
+              <img src={item.download_url} width="200" />
             </div>
-          })}
-        </div>
+          )
+        })}
       </div>
     </div>
   )
